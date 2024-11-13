8306d05b38a4b8ac5304d9076f381003a99de5da
Md. Morsalin <mdmorsalin.hstu@gmail.com>
2024-11-13 06:46:26 +0600

    Update PythonBasic.py

--- PythonBasic.py
+++ PythonBasic.py
@@ -1,3 +1,10 @@
 print("Hello World! This is Morsalin here.")
 for i in range(5) :
   print("Morsalin")
+
+#finding the even and odd numbers
+a = int(input("Give a number: "))
+if a % 2 == 0:
+  print(str(a) + " is a even number")
+else:
+  print(str(a) + " is a odd number")
