16ff7d9e51ed8ef2b4398c622760b5db9f6e33cc
Md. Morsalin <mdmorsalin.hstu@gmail.com>
2024-11-13 07:12:44 +0600

    Update PythonBasic.py

--- PythonBasic.py
+++ PythonBasic.py
@@ -8,3 +8,22 @@ if a % 2 == 0:
   print(str(a) + " is a even number")
 else:
   print(str(a) + " is a odd number")
+#finding the smallest and largest number among three
+a = 9
+b = 8
+c = 10
+if (a > b) & (a > c):
+    print(str(a) + " is largest number")
+elif (b > a) & (b > c):
+    print(str(b) + "  is the largest number")
+else:
+    print(str(c) + " is the largest number")
+
+if (a < b) & (a < c) :
+    print(str(a) + " is the smallest number")
+elif (b < a) & (b < c) :
+    print(str(b) + " is the smallest number")
+else :
+    print(str(c) + " is the smallest number")
+
+#That's done
